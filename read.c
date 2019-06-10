c05a7cf9e64c2f00dbe356788773d49a41536002
mkoch <matti@sagthallo.de>
2019-06-10 09:49:56 +0200

    added files

---
+++ read.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <editline/readline.h>
+
+int main(int argc, char** argv) {
+	puts("Lispy version 0.0.0.0.1");
+	puts("Press Ctrl-C to exit");
+	while(1) {
+		char* input = readline("lispy> ");
+		add_history(input);
+		printf("No, you're a %s!", input);
+		free(input);
+	}
+	return 0;
+}
