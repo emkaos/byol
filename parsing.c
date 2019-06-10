0d62c49686c57ceb6397eee82aa713e5896edb12
mkoch <matti@sagthallo.de>
2019-06-10 14:49:38 +0200

    evaluation

--- parsing.c
+++ parsing.c
@@ -3,6 +3,36 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
+long eval_op(long a1, char* op, long a2) {
+	if (strcmp(op, "+") == 0) { return a1 + a2; }
+	if (strcmp(op, "-") == 0) { return a1 - a2; }
+	if (strcmp(op, "*") == 0) { return a1 * a2; }
+	if (strcmp(op, "/") == 0) { return a1 / a2; }
+	if (strcmp(op, "%") == 0) { return a1 % a2; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+	/* Number */
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	/* operator is always second child */
+	char* op = t->children[1]->contents;
+
+	/* first argument to operator */
+	long x = eval(t->children[2]);
+
+	int i=3;
+	while(strstr(t->children[i]->tag, "expression")) {
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
 int main(int argc, char** argv) {
 	mpc_parser_t* Number = mpc_new("number");
 	mpc_parser_t* Operator = mpc_new("operator");
@@ -10,14 +40,14 @@ int main(int argc, char** argv) {
 	mpc_parser_t* Lispy = mpc_new("lispy");
 
 	mpca_lang(MPCA_LANG_DEFAULT, "\
-			number		: /-?[0-9]+(.[0-9]+)?/ ;\
+			number		: /-?[0-9]+/ ;\
 			operator	: '+' | '-' | '*' | '/' | '%' ;\
 			expression	: <number> | '(' <operator> <expression>+ ')';\
 			lispy		: /^/ <operator> <expression>+ /$/ ;\
 			", 
 			Number, Operator, Expression, Lispy);
 
-	puts("Lispy version 0.0.0.0.1");
+	puts("Lispy version 0.0.0.0.3");
 	puts("Press Ctrl-C to exit");
 	while(1) {
 		char* input = readline("lispy> ");
@@ -26,6 +56,10 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
 			mpc_ast_print(r.output);
+
+			long result = eval(r.output);
+			printf("%li\n", result);
+
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
