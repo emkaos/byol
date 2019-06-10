3bf8f0c8d0b2daed538efd74a189ae662e538ca6
Matthias Koch <matthiaskoch@Matthiass-MacBook-Pro.local>
2019-06-10 10:11:24 +0200

    add linebreak

--- read.c
+++ read.c
@@ -8,7 +8,8 @@ int main(int argc, char** argv) {
 	while(1) {
 		char* input = readline("lispy> ");
 		add_history(input);
-		printf("No, you're a %s!", input);
+
+		printf("No, you're a %s!\n", input);
 		free(input);
 	}
 	return 0;
